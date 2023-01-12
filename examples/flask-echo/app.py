d6299c5fe813fa9621869630872b866f702302e3
jack60810 <jack608102@gmail.com>
2023-01-12 22:11:40 +0800

    Update app.py

--- examples/flask-echo/app.py
+++ examples/flask-echo/app.py
@@ -70,7 +70,7 @@ def callback():
         print("user_id =", user_id)
         print("type =",event.source.type )
         print(type(event.source.type ))
-        if user_id == 'U51456d8305d41ebe90d67fd3db3a7c59' :#& typee == 'user': 
+        if user_id == 'U51456d8305d41ebe90d67fd3db3a7c59' & event.source.type == 'user' :#& typee == 'user': 
             tt = event.message.text
             try:
                 line_bot_api.push_message('U51456d8305d41ebe90d67fd3db3a7c59', TextSendMessage(text=tt+' orz')) #Cb9601c1295de74756ab8280899e15c92
