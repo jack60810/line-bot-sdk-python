adec05d63e0c073cd1ddad6d1bf922b4a14f7af3
jack60810 <jack608102@gmail.com>
2023-07-08 11:45:55 +0800

    update files

---
+++ examples/flask-echo-test/app.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+
+import os
+import sys
+from argparse import ArgumentParser
+import openai
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookParser
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+openai_key = os.getenv('openai_key', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+parser = WebhookParser(channel_secret)
+
+
+# Set up OpenAI credentials
+openai.api_key = openai_key
+openai_model = 'gpt-3.5-turbo'  # or use 'davinci-codex' for a more focused model
+
+# Initialize chat history
+chat_history = []
+
+
+# Define the chatbot function
+def chatbot(message):
+    # Append the user's message to the chat history
+    if len(chat_history) >= 5:
+        chat_history.pop(0)
+    # Append the user's message to the chat history
+    chat_history.append({'role': 'user', 'content': message})
+    # Generate response using ChatGPT
+    response = openai.ChatCompletion.create(
+        model=openai_model,
+        messages=chat_history,
+        max_tokens = 1000,
+        top_p = 1
+    ).choices[0].message.content.strip()
+
+    # Append the chatbot's response to the chat history
+    chat_history.append({'role': 'system', 'content': response})
+
+    return response
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=chatbot(event.message.text))
+        )
+
+    return 'OK'
+
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    app.run(debug=options.debug, port=options.port)
